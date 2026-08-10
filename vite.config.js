3baf24a543ccc8d5bbcd80a16588998eaa027ce4
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-08-10 02:39:47 +0100

    Add login.html to Rollup input configuration

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        // Additional pages will be added here as they are created
+        login: resolve(__dirname, 'login.html'),
       }
     }
   },
