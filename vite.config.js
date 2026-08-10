d5319b327d695c333bc6639c887da0cd7fdf0c8c
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-08-10 02:49:10 +0100

    Add dashboard.html to Vite config input

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'login.html'),
         register: resolve(__dirname, 'register.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
       }
     }
   }
