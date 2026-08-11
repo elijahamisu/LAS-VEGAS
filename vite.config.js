23da57db8f8e8dc27f8d37cc429f4d3a69bbae82
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-08-11 21:44:31 +0100

    Add my_investment entry to Vite config
    Added a new entry for 'my_investment' in the Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
         notifications: resolve(__dirname, 'notifications.html'),
         profile: resolve(__dirname, 'profile.html'),
         support: resolve(__dirname, 'support.html'),
+        my_investment: resolve(__dirname, 'my-investment.html'),
         // Admin Entry
         admin_login: resolve(__dirname, 'admin/login.html'),
         admin_dashboard: resolve(__dirname, 'admin/index.html'),
