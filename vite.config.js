26b5af8dd47a4ca1971faba1cd5905613e3ff9ac
elijahamisu <amisuelijaholuwasegun@gmail.com>
2026-08-14 02:36:22 +0100

    Replace nekpay with glopayment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
         admin_referrals: resolve(__dirname, 'admin/referrals.html'),
         admin_gift_codes: resolve(__dirname, 'admin/gift-codes.html'),
         admin_notifications: resolve(__dirname, 'admin/notifications.html'),
-        admin_nekpay: resolve(__dirname, 'admin/nekpay.html'),
+        admin_glopayment: resolve(__dirname, 'admin/glopayment.html'),
         admin_payout_accounts: resolve(__dirname, 'admin/payout-accounts.html'),
         admin_reports: resolve(__dirname, 'admin/reports.html'),
         admin_settings: resolve(__dirname, 'admin/settings.html'),
